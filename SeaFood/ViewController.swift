a6b6cd7dbc12a87f51fe82f48652cfca19915663
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-11-21 21:57:31 +0900

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Anastasia Grey on 2024/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
